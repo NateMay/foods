0ce272b5f220ff36aa1ac25ab90e95d50739ac0e
nate <nathaniel.may22@gmail.com>
2021-03-27 20:48:56 -0400

    many to many batch script added

--- foods/models.py
+++ foods/models.py
@@ -3,29 +3,39 @@ from django.core.validators import MinLengthValidator
 
 # Many-to-many relationships - https://docs.djangoproject.com/en/3.1/topics/db/examples/many_to_many/
 
+
 class WikiScrapeCategory(models.Model):
     name = models.CharField(
-            max_length=200,
-            blank=False,
-            validators=[MinLengthValidator(2, "Category names must be longer than 1 character")]
+        max_length=200,
+        blank=False,
+        validators=[MinLengthValidator(
+            2, "Category names must be longer than 1 character")]
     )
     description = models.CharField(max_length=30000)
     wiki_url = models.CharField(max_length=1000)
-    parent_category = models.ForeignKey('WikiScrapeCategory', on_delete=models.SET_NULL, null=True)
+    # parent_category = models.ForeignKey(
+    #     'WikiScrapeCategory', on_delete=models.SET_NULL, null=True)
+    foods = models.ManyToManyField('WikiScrapeFood', through='CategoryAssignment')
 
     def __str__(self):
         return self.name
+
 
 class WikiScrapeFood(models.Model):
     name = models.CharField(
-            max_length=200,
-            blank=False,
-            validators=[MinLengthValidator(2, "Food names must be longer than 1 character")]
+        max_length=200,
+        blank=False,
+        validators=[MinLengthValidator(2, "Food names must be longer than 1 character")]
     )
     description = models.CharField(max_length=30000)
     wiki_url = models.CharField(max_length=1000)
-    img_src = models.CharField(max_length=1000)
-    categories = models.ManyToManyField(WikiScrapeCategory)
+    img_src = models.CharField(max_length=1000, null=True)
+    categories = models.ManyToManyField('WikiScrapeCategory', through='CategoryAssignment')
 
     def __str__(self):
         return self.name
+
+
+class CategoryAssignment(models.Model):
+    food = models.ForeignKey(WikiScrapeFood, on_delete=models.CASCADE)
+    category = models.ForeignKey(WikiScrapeCategory, on_delete=models.CASCADE)
---
+++ scripts/wiki_scrape_load.py
@@ -0,0 +1,73 @@
+# cd ~/django_projects/mysite/batch
+# python3 manage.py runscript wiki_scrape_load
+
+from numpy import isnan
+from foods.models import WikiScrapeCategory, WikiScrapeFood, CategoryAssignment
+import pandas as pd
+import sqlite3
+from pydash import py_
+import os
+
+# stores the category with the original id for later lookup
+def rip_category_data(wiki_categories):
+    categories = []
+    for _, row in wiki_categories.iterrows():
+        categories.append({
+            'id': row.id,
+            'category': WikiScrapeCategory.objects.get_or_create(
+                name=row['name'],
+                description=row['description'],
+                wiki_url=row['wiki_url'])[0],
+            'parent_id': int(row.parent_category) if ~isnan(row.parent_category) else None
+        })
+    return categories
+
+# stores the food with the original id for later lookup
+def rip_food_data(wiki_foods):
+    foods = []
+    for _, row in wiki_foods.iterrows():
+        foods.append({
+            'id': row.id,
+            'food': WikiScrapeFood.objects.get_or_create(
+                name=row['name'],
+                description=row['description'],
+                wiki_url=row['wiki_url'],
+                img_src=row['image_src']
+            )[0],
+        })
+    return foods
+
+
+def delete_all():
+    try:
+        WikiScrapeCategory.objects.all().delete()
+        WikiScrapeFood.objects.all().delete()
+        CategoryAssignment.objects.all().delete()
+    except Exception:
+        print('no object yet')
+        pass
+
+def run():
+    # setup
+    connection = sqlite3.connect(os.path.abspath('.') + '/wiki_scrape/food_data.sqlite')
+    delete_all()
+
+    # intermediate data structures for bridging the M2M relationships
+    categories = rip_category_data(pd.read_sql_query(
+        "SELECT * from wiki_category", connection))
+
+    foods = rip_food_data(pd.read_sql_query(
+        "SELECT * from wiki_food", connection))
+
+    # loop through wiki_food_category to create the Junction Table
+    for _, row in pd.read_sql_query("SELECT * from wiki_food_category", connection).iterrows():
+        
+
+        a = CategoryAssignment(
+            # Use the id values in wiki_food_category to lookup the food and category 
+            # objects created in rip_category_data() & rip_food_data()
+            food=py_.find(foods, lambda f: f.get('id') == row.food_id).get('food'),
+            category=py_.find(categories, lambda c: c.get('id') == row.category_id).get('category'),
+        )
+        # !!! ERROR: django.db.utils.OperationalError: no such table: foods_categoryassignment
+        a.save()
